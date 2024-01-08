bafa76cb36da104f6139f63a22b0e8a5d447f357
Anvar <anvar.ganiev99@gmail.com>
2024-01-08 14:10:48 -0500

    All Files
    Forgot to create a repository before beginning the project.

---
+++ MyForm.cpp
@@ -0,0 +1,13 @@
+#include "MyForm.h"
+
+using namespace System;
+using namespace System::Windows::Forms;
+
+[STAThreadAttribute]
+
+void main(array<String^>^ args) {
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+	$safeprojectname$::MyForm form;
+	Application::Run(% form);
+}
